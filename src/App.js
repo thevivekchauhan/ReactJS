ffbd7d31b2197bc6a70a678109e1e5bef4def4a5
Vivek Chauhan <vivekchauhan251002@gmail.com>
2025-06-30 15:54:37 +0530

    install node module and change in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import FunctionalCompo from './Component/pages/2-functional-compo/functional-compo';
+// import FunctionalCompo from './Component/pages/2-functional-compo/functional-compo';
 // import Classcompo from './Component/1-class-compo/pages/class-compo';
+import Props from './Component/pages/3-props/props'
+
 
 function App() {
   return (
     <div className="App">
-      <FunctionalCompo />
+      {/* <FunctionalCompo /> */}
       {/* <Classcompo /> */}
+      </Props>
     </div>
   );
 }
