7c2bb17aa25ca8ec6485dc7892b6199a60ff7e08
Vivek Chauhan <vivekchauhan251002@gmail.com>
2025-07-01 10:06:21 +0530

    New folder 4-function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 // import FunctionalCompo from './Component/pages/2-functional-compo/functional-compo';
 // import Classcompo from './Component/1-class-compo/pages/class-compo';
-import Props from './Component/pages/3-props/props';
+// import Props from './Component/pages/3-props/props';
+import Allfunction from './Component/pages/4-function/function';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalCompo /> */}
       {/* <Classcompo /> */}
-      <Props />
+      {/* {/* <Props />/} */}
+      <Allfunction />
     </div>
   );
 }
---
+++ src/Component/pages/4-function/function.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AllFunction (){
+    return(
+        <div>
+            <h1>Function Component</h1>
+        </div>
+    )
+}
+
+export default AllFunction;
